545c8be122fb4b3884929d0100b56bff46d4326d
Takeshi HASEGAWA <hasegaw@gmail.com>
2014-12-09 22:04:30 +0900

    - Eliminated R script errors - added scale

--- visualize_prociostat.R
+++ visualize_prociostat.R
@@ -17,10 +17,10 @@ cpurate_lim = c(0, max(csv$delta_utime, csv$delta_stime) * 1.1);
 
 par(oma = c(0, 0, 0, 2))
 par(pch=20)
-plot(x=csv$time, y=csv$delta_write_bytes, xlab="", ylab="", ylim=iorate_lim, col='pink', axes = FALSE);
+plot(x=csv$time, y=csv$delta_write_bytes, xlab="", ylab="KB/s", ylim=iorate_lim, col='pink', axes = FALSE);
 par(new = TRUE)
 plot(x=csv$time, y=csv$delta_read_bytes, xlab="", ylab="", ylim=iorate_lim, col='lightgreen', axes = FALSE);
-mtext(src_csv, size=4, line=3)
+mtext('cpu%', side=4, line=3)
 
 axis(1) # draw X axis
 axis(2) # draw Y axis
